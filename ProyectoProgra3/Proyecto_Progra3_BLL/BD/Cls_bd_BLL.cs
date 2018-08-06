8624af52f4653c675e8d63ed89a704f38face877
jorge <jorike0787@gmail.com>
2018-08-06 13:29:34 -0600

    Creo que ya esta el adapter y la conexion no los pude probar la comida me llama

---
+++ ProyectoProgra3/Proyecto_Progra3_BLL/BD/Cls_bd_BLL.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using Proyecto_Progra3_DAL.BD;
+
+namespace Proyecto_Progra3_BLL.BD
+{
+    public class Cls_bd_BLL
+    {
+        private void Conexion(ref Cls_bd_DAL Obj_bd_DAL)
+        {
+            Obj_bd_DAL.scadena = ConfigurationManager.ConnectionStrings["Win_Aut"].ToString().Trim();
+            Obj_bd_DAL.Obj_sql_conexion = new SqlConnection(Obj_bd_DAL.scadena);
+        }
+
+        public void Ejecutar_adapter(ref Cls_bd_DAL Obj_bd_DAL)
+        {
+            try
+            {
+                if (Obj_bd_DAL.Obj_sql_conexion.State == System.Data.ConnectionState.Closed)
+                {
+                    Obj_bd_DAL.Obj_sql_conexion.Open();
+                    Obj_bd_DAL.Obj_sql_adap = new SqlDataAdapter(Obj_bd_DAL.ssentencia, Obj_bd_DAL.Obj_sql_conexion);
+                    Obj_bd_DAL.Obj_sql_adap.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+
+                    Obj_bd_DAL.Obj_sql_adap.Fill(Obj_bd_DAL.Data_set, Obj_bd_DAL.snombretabla);
+                }
+                Obj_bd_DAL.smsjError = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                Obj_bd_DAL.smsjError = ex.ToString();
+            }
+            finally
+            {
+                if (Obj_bd_DAL.Obj_sql_conexion != null)
+                {
+                    if (Obj_bd_DAL.Obj_sql_conexion.State == System.Data.ConnectionState.Open)
+                    {
+                        Obj_bd_DAL.Obj_sql_conexion.Close();
+                    }
+                    Obj_bd_DAL.Obj_sql_conexion.Dispose();
+                }
+            }
+          
+        }
+
+    }
+}
