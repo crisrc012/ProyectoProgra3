b15d37b1f7a2fbe8bcb189b206b36595e871d8f8
Carlos Torres <ctorres@ex2outcoding.local>
2018-08-08 23:16:53 -0600

    Solucion diferente creada el lunes como propuesta alterna.

---
+++ ProyectoProgra3/Programacion.Proyecto.Biz/ActivosService.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using Programacion.Proyecto.DataAccess;
+
+namespace Programacion.Proyecto.Biz
+{
+    public class ActivosService
+    {
+        public DataTable GetActivos()
+        {
+            using (var context = SqlDbContext.Create())
+            {
+                return context.ExecuteDataTable("SP_LISTAR_ACTIVOS");
+            }
+        }
+    }
+}
---
+++ ProyectoProgra3/Programacion.Proyecto.UI/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Programacion.Proyecto.Biz;
+
+namespace Programacion.Proyecto.UI
+{
+    public partial class Form1 : Form
+    {
+        private readonly ActivosService _activosService;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _activosService = new ActivosService();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = _activosService.GetActivos();
+        }
+    }
+}
