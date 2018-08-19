6982ac90ec594a5cb1f7aa8826262d4c82b72e1c
Wilberth <Wilberth@VYANTY>
2018-08-19 03:45:00 -0600

    Turnos

--- Proyecto_call_PL/Turnos/frm_editar_turnos_PL.Designer.cs
+++ Proyecto_call_PL/Turnos/frm_editar_turnos_PL.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_editar_turnos_PL));
             this.gr_Turnos = new System.Windows.Forms.GroupBox();
             this.txt_Id_Turno = new System.Windows.Forms.TextBox();
             this.lbl_Id_Turno = new System.Windows.Forms.Label();
@@ -48,6 +49,8 @@
             // 
             // gr_Turnos
             // 
+            this.gr_Turnos.BackColor = System.Drawing.Color.PowderBlue;
+            this.gr_Turnos.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.gr_Turnos.Controls.Add(this.txt_Id_Turno);
             this.gr_Turnos.Controls.Add(this.lbl_Id_Turno);
             this.gr_Turnos.Controls.Add(this.txt_Hora_Salida);
@@ -60,81 +63,100 @@
             this.gr_Turnos.Controls.Add(this.lbl_Descripcion);
             this.gr_Turnos.Controls.Add(this.lbl_Estado);
             this.gr_Turnos.Controls.Add(this.cmb_Estado);
+            this.gr_Turnos.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.gr_Turnos.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gr_Turnos.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             this.gr_Turnos.Location = new System.Drawing.Point(12, 12);
             this.gr_Turnos.Name = "gr_Turnos";
             this.gr_Turnos.Size = new System.Drawing.Size(305, 246);
-            this.gr_Turnos.TabIndex = 9;
+            this.gr_Turnos.TabIndex = 14;
             this.gr_Turnos.TabStop = false;
-            this.gr_Turnos.Text = "Turnos";
+            this.gr_Turnos.Text = "Turno";
             // 
             // txt_Id_Turno
             // 
+            this.txt_Id_Turno.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Id_Turno.Location = new System.Drawing.Point(97, 31);
             this.txt_Id_Turno.Name = "txt_Id_Turno";
             this.txt_Id_Turno.Size = new System.Drawing.Size(174, 20);
-            this.txt_Id_Turno.TabIndex = 11;
+            this.txt_Id_Turno.TabIndex = 0;
             // 
             // lbl_Id_Turno
             // 
             this.lbl_Id_Turno.AutoSize = true;
+            this.lbl_Id_Turno.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Id_Turno.ForeColor = System.Drawing.Color.DarkBlue;
             this.lbl_Id_Turno.Location = new System.Drawing.Point(16, 34);
             this.lbl_Id_Turno.Name = "lbl_Id_Turno";
-            this.lbl_Id_Turno.Size = new System.Drawing.Size(47, 13);
-            this.lbl_Id_Turno.TabIndex = 10;
+            this.lbl_Id_Turno.Size = new System.Drawing.Size(55, 13);
+            this.lbl_Id_Turno.TabIndex = 8;
             this.lbl_Id_Turno.Text = "Id Turno";
             // 
             // txt_Hora_Salida
             // 
+            this.txt_Hora_Salida.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Hora_Salida.Location = new System.Drawing.Point(97, 165);
+            this.txt_Hora_Salida.MaxLength = 10;
             this.txt_Hora_Salida.Name = "txt_Hora_Salida";
             this.txt_Hora_Salida.Size = new System.Drawing.Size(174, 20);
-            this.txt_Hora_Salida.TabIndex = 9;
+            this.txt_Hora_Salida.TabIndex = 4;
             // 
             // txt_Hora_Entrada
             // 
+            this.txt_Hora_Entrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Hora_Entrada.Location = new System.Drawing.Point(97, 135);
+            this.txt_Hora_Entrada.MaxLength = 10;
             this.txt_Hora_Entrada.Name = "txt_Hora_Entrada";
             this.txt_Hora_Entrada.Size = new System.Drawing.Size(174, 20);
-            this.txt_Hora_Entrada.TabIndex = 8;
+            this.txt_Hora_Entrada.TabIndex = 3;
             // 
             // txt_Cant_Horas
             // 
+            this.txt_Cant_Horas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Cant_Horas.Location = new System.Drawing.Point(97, 101);
             this.txt_Cant_Horas.Name = "txt_Cant_Horas";
             this.txt_Cant_Horas.Size = new System.Drawing.Size(174, 20);
-            this.txt_Cant_Horas.TabIndex = 7;
+            this.txt_Cant_Horas.TabIndex = 2;
             this.txt_Cant_Horas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Cant_Horas_KeyPress);
             // 
             // lbl_Hora_Salida
             // 
             this.lbl_Hora_Salida.AutoSize = true;
+            this.lbl_Hora_Salida.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Hora_Salida.ForeColor = System.Drawing.Color.DarkBlue;
             this.lbl_Hora_Salida.Location = new System.Drawing.Point(19, 172);
             this.lbl_Hora_Salida.Name = "lbl_Hora_Salida";
-            this.lbl_Hora_Salida.Size = new System.Drawing.Size(62, 13);
-            this.lbl_Hora_Salida.TabIndex = 6;
+            this.lbl_Hora_Salida.Size = new System.Drawing.Size(73, 13);
+            this.lbl_Hora_Salida.TabIndex = 12;
             this.lbl_Hora_Salida.Text = "Hora Salida";
             // 
             // lbl_Hora_Entrada
             // 
             this.lbl_Hora_Entrada.AutoSize = true;
+            this.lbl_Hora_Entrada.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Hora_Entrada.ForeColor = System.Drawing.Color.DarkBlue;
             this.lbl_Hora_Entrada.Location = new System.Drawing.Point(19, 138);
             this.lbl_Hora_Entrada.Name = "lbl_Hora_Entrada";
-            this.lbl_Hora_Entrada.Size = new System.Drawing.Size(70, 13);
-            this.lbl_Hora_Entrada.TabIndex = 5;
+            this.lbl_Hora_Entrada.Size = new System.Drawing.Size(82, 13);
+            this.lbl_Hora_Entrada.TabIndex = 11;
             this.lbl_Hora_Entrada.Text = "Hora Entrada";
             // 
             // lbl_Cant_Horas
             // 
             this.lbl_Cant_Horas.AutoSize = true;
+            this.lbl_Cant_Horas.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Cant_Horas.ForeColor = System.Drawing.Color.DarkBlue;
             this.lbl_Cant_Horas.Location = new System.Drawing.Point(19, 104);
             this.lbl_Cant_Horas.Name = "lbl_Cant_Horas";
-            this.lbl_Cant_Horas.Size = new System.Drawing.Size(63, 13);
-            this.lbl_Cant_Horas.TabIndex = 4;
+            this.lbl_Cant_Horas.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Cant_Horas.TabIndex = 10;
             this.lbl_Cant_Horas.Text = "Cant. Horas";
             // 
             // txt_Descripcion
             // 
+            this.txt_Descripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Descripcion.Location = new System.Drawing.Point(97, 66);
+            this.txt_Descripcion.MaxLength = 65;
             this.txt_Descripcion.Name = "txt_Descripcion";
             this.txt_Descripcion.Size = new System.Drawing.Size(174, 20);
             this.txt_Descripcion.TabIndex = 1;
@@ -142,61 +164,76 @@
             // lbl_Descripcion
             // 
             this.lbl_Descripcion.AutoSize = true;
+            this.lbl_Descripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Descripcion.ForeColor = System.Drawing.Color.DarkBlue;
             this.lbl_Descripcion.Location = new System.Drawing.Point(16, 69);
             this.lbl_Descripcion.Name = "lbl_Descripcion";
-            this.lbl_Descripcion.Size = new System.Drawing.Size(63, 13);
-            this.lbl_Descripcion.TabIndex = 0;
+            this.lbl_Descripcion.Size = new System.Drawing.Size(74, 13);
+            this.lbl_Descripcion.TabIndex = 9;
             this.lbl_Descripcion.Text = "Descripción";
             // 
             // lbl_Estado
             // 
             this.lbl_Estado.AutoSize = true;
+            this.lbl_Estado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Estado.ForeColor = System.Drawing.Color.DarkBlue;
             this.lbl_Estado.Location = new System.Drawing.Point(19, 202);
             this.lbl_Estado.Name = "lbl_Estado";
-            this.lbl_Estado.Size = new System.Drawing.Size(40, 13);
-            this.lbl_Estado.TabIndex = 3;
+            this.lbl_Estado.Size = new System.Drawing.Size(46, 13);
+            this.lbl_Estado.TabIndex = 13;
             this.lbl_Estado.Text = "Estado";
             // 
             // cmb_Estado
             // 
             this.cmb_Estado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_Estado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmb_Estado.FormattingEnabled = true;
             this.cmb_Estado.Location = new System.Drawing.Point(97, 199);
             this.cmb_Estado.Name = "cmb_Estado";
             this.cmb_Estado.Size = new System.Drawing.Size(174, 21);
-            this.cmb_Estado.TabIndex = 2;
+            this.cmb_Estado.TabIndex = 5;
             // 
             // btn_Aceptar
             // 
+            this.btn_Aceptar.BackColor = System.Drawing.Color.PowderBlue;
+            this.btn_Aceptar.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_Aceptar.BackgroundImage")));
+            this.btn_Aceptar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_Aceptar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Aceptar.Image = ((System.Drawing.Image)(resources.GetObject("btn_Aceptar.Image")));
             this.btn_Aceptar.Location = new System.Drawing.Point(160, 264);
             this.btn_Aceptar.Name = "btn_Aceptar";
-            this.btn_Aceptar.Size = new System.Drawing.Size(75, 23);
-            this.btn_Aceptar.TabIndex = 8;
-            this.btn_Aceptar.Text = "Aceptar";
-            this.btn_Aceptar.UseVisualStyleBackColor = true;
+            this.btn_Aceptar.Size = new System.Drawing.Size(75, 37);
+            this.btn_Aceptar.TabIndex = 6;
+            this.btn_Aceptar.UseVisualStyleBackColor = false;
             this.btn_Aceptar.Click += new System.EventHandler(this.btn_Aceptar_Click);
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.PowderBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
             this.button1.Location = new System.Drawing.Point(241, 264);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "Salir";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 7;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // frm_editar_turnos_PL
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(328, 307);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(328, 309);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.gr_Turnos);
             this.Controls.Add(this.btn_Aceptar);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "frm_editar_turnos_PL";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Turnos";
+            this.Text = "Modificar y Agregar";
             this.Load += new System.EventHandler(this.frm_editar_turnos_PL_Load);
             this.gr_Turnos.ResumeLayout(false);
             this.gr_Turnos.PerformLayout();
